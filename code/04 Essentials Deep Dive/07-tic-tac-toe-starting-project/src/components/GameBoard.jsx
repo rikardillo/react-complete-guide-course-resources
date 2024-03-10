479dd0bc47dc38aff64ca1c181a635e1026a2d8f
Ricardo Domínguez <ricardomdominguezr@gmail.com>
2024-03-10 15:20:56 -0400

    Added GameBoard
    Dynamically created GameBoard

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,7 +1,28 @@
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
 export default function () {
   return (
-    <div>
-      
-    </div>
+    <ol id="game-board">
+      {initialGameBoard.map(
+        (
+          row,
+          rowIndex // First parameter of map() Method is the element of the array to be mapped, the second parameter is the index
+        ) => (
+          <li key={rowIndex}>
+            <ol>
+              {row.map((playerSymbol, colIndex) => (
+                <li key={colIndex}>
+                  <button>{playerSymbol}</button>
+                </li>
+              ))}
+            </ol>
+          </li>
+        )
+      )}
+    </ol>
   );
-};
+}
