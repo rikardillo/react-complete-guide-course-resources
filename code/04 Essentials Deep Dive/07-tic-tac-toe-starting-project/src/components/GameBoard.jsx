bae956f65c0deb998b48c212adbe4dc8f5a3e7cf
Ricardo Domínguez <ricardomdominguezr@gmail.com>
2024-03-11 09:51:48 -0400

    Added Active Player Activation

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export default function () {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      // Update the state in an immutable way
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+      return updatedBoard;
+    });
+
+    onSelectSquare();
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map(
+      {gameBoard.map(
         (
           row,
           rowIndex // First parameter of map() Method is the element of the array to be mapped, the second parameter is the index
@@ -16,7 +33,11 @@ export default function () {
             <ol>
               {row.map((playerSymbol, colIndex) => (
                 <li key={colIndex}>
-                  <button>{playerSymbol}</button>
+                  <button
+                    onClick={() => handleSelectSquare(rowIndex, colIndex)}
+                  >
+                    {playerSymbol}
+                  </button>
                 </li>
               ))}
             </ol>
