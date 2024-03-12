dd90b3f5548b586fd607a7c1c3098a99f140d42c
Ricardo Domínguez <ricardomdominguezr@gmail.com>
2024-03-12 10:05:25 -0400

    Added Winning Combinations

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,37 +1,9 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
 
-export default function GameBoard({ onSelectSquare, turns }) {
-  let gameBoard = initialGameBoard;
-
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-
-  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  // function handleSelectSquare(rowIndex, colIndex) {
-  //   setGameBoard((prevGameBoard) => {
-  //     // Update the state in an immutable way
-  //     const updatedBoard = [
-  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
-  //     ];
-  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //     return updatedBoard;
-  //   });
-
-  //   onSelectSquare();
-  // }
-
+export default function GameBoard({ onSelectSquare, board }) {
+  
   return (
     <ol id="game-board">
-      {gameBoard.map(
+      {board.map(
         (
           row,
           rowIndex // First parameter of map() Method is the element of the array to be mapped, the second parameter is the index
@@ -40,7 +12,9 @@ export default function GameBoard({ onSelectSquare, turns }) {
             <ol>
               {row.map((playerSymbol, colIndex) => (
                 <li key={colIndex}>
-                  <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                  <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null} >
+                    {playerSymbol}
+                  </button>
                 </li>
               ))}
             </ol>
